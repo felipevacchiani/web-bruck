39e44598607f4fcbfaa61776b1f8f97f942ce820
felipevacchiani <70178148+felipevacchiani@users.noreply.github.com>
2026-03-30 17:38:19 -0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -78,8 +78,3 @@ export const config = {
     '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
 }
-export const config = {
-  matcher: [
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
-  ],
-}
