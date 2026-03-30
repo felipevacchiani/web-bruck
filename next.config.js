27380f5c9956b6c134c5d872b1c00da8026c4bdd
felipevacchiani <70178148+felipevacchiani@users.noreply.github.com>
2026-03-30 20:52:18 -0300

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
+  async headers() {
+    return [
+      {
+        source: '/view/:path*',
+        headers: [
+          { key: 'X-Frame-Options', value: 'SAMEORIGIN' },
+          { key: 'Content-Security-Policy', value: "frame-ancestors 'self'" },
+        ],
+      },
+    ]
+  },
+}
+
+module.exports = nextConfig
