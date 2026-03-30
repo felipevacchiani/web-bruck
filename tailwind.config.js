477665994a47572d5f495c9ded8c8387500d60d9
felipevacchiani <70178148+felipevacchiani@users.noreply.github.com>
2026-03-30 18:26:15 -0300

    Update and rename tailwind.config.ts to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -17,6 +16,8 @@ const config: Config = {
       },
     },
   },
+  plugins: [],
+}
   plugins: [],
 }
 
